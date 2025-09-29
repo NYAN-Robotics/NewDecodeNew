e6bb5383487d89cf57bd9445c05f84a5cc1f7161
laquishabobson <boogerbong1@gmail.com>
2025-09-29 15:55:00 -0500

    We made all of the classes and folders to format it

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/Test.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.opmodes.Teleop;
+
+public class Test {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/subsystems/TestBench.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.utilities.subsystems;
+
+public class TestBench {
+}
